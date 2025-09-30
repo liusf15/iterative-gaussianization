9b6c50d22199338de66a94125ab1ff5a3f59405d
liusf15 <sliu@flatironinstitute.org>
2025-09-30 15:26:13 -0400

    cleanup

--- src/projection_vi/__init__.py
+++ src/projection_vi/__init__.py
@@ -1,4 +1,3 @@
-from .flows import ComponentwiseFlow, AffineFlow, RealNVP, NeuralSplineFlow
-from .train import train
+from .flows import ComponentwiseFlow, AffineFlow, NeuralSplineFlow
 from .iterative_gaussianization import iterative_gaussianization, iterative_forward_map, MFVIStep, apply_householder_transpose, apply_householder
 from . import utils
